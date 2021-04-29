df1943431b0cf5235dfb9ebf1e2061326af059aa
Edson Cândido Alves Júnior <edson.c.a.j@hotmail.com>
2021-04-29 12:14:52 -0300

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist//out-tsc/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/web'));
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/out-tsc/<name-of-app>/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/web/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
